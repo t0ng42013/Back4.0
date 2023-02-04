63fbe252df5b33181ef7373f9a3f2a7f783fb9da
t0ng42013 <tonga88@live.com.ar>
2023-02-03 22:39:03 -0300

    confiCors

---
+++ src/main/java/com/portfolio/LGA/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.portfolio.LGA.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsCongigurer(){
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/login")
+                        .allowedOrigins("http://localhost:4200", "lga-portfolio.web.app")
+                        .allowedMethods("*")
+                        .exposedHeaders("*");
+
+
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:4200", "lga-portfolio.web.app")
+                        .allowedMethods("*");
+            }
+
+        };
+    }
+}
